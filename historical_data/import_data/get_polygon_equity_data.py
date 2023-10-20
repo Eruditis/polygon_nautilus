6b30901d95591e2c939b38b565131c95ec3f4e7d
amitsinghpgs <32765015+amitsinghpgs@users.noreply.github.com>
2023-10-20 08:55:18 +0530

    add instrumentstatus to backtest engine (#3)
    Changes verified, nice work!

--- historical_data/import_data/get_polygon_equity_data.py
+++ historical_data/import_data/get_polygon_equity_data.py
@@ -1,7 +1,13 @@
+"""
+Usage:
+python historical_data/import_data/get_polygon_equity_data.py
+"""
 import config
 from datetime import date
 import logging
+import pandas as pd
 from historical_data.import_data.polygon_equity_data import PolygonEquityData
+from nautilus_trader.model.data import Bar
 
 logging.basicConfig(level=logging.INFO)
 # test PolygonEquityData class
@@ -9,5 +15,8 @@ poly = PolygonEquityData(config, ["QQQ"], "/workspaces/data")
 poly.get_tickers()
 poly.get_bar_data_for_tickers(date(2023, 9, 16), date(2023, 9, 30))
 
-bar_df = poly.read_catalog_data()
-print(bar_df.head())
+# read from catalog data
+bars = poly.read_catalog_bars()
+print(bars)
+df_bars = pd.DataFrame([Bar.to_dict(b) for b in bars])
+print(df_bars)
